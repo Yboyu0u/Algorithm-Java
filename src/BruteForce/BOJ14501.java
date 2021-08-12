2a2ce49949aa876e8b2386047add440526c7f205
유영우 <fbduddn97@naver.com>
2021-08-13 00:45:58 +0900

    [add] BOJ14501 in BruteForce

--- src/BruteForce/BOJ14501.java
+++ src/BruteForce/BOJ14501.java
@@ -1,27 +1,18 @@
 package BruteForce;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 // 백준이의 퇴사
 // 오늘부터 n+1일째 되는 날 퇴사를 하기 위해서,
 // 완료 시간과 금액
 // 백준이가 얻을 수 있는 최대 수익을 구하는 프로그램
 
-class Consult{
-    int time;
-    int pay;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    public Consult(int time, int pay) {
-        this.time = time;
-        this.pay = pay;
-    }
-}
-
-public class BOJ14501 {
+class Main {
     static ArrayList<Consult> schedule = new ArrayList<>();
     static int sum;
     static int n;
+    static ArrayList<Integer> arr = new ArrayList<>();
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -31,11 +22,28 @@ public class BOJ14501 {
             Consult c = new Consult(sc.nextInt(),sc.nextInt());
             schedule.add(c);
         }
-        getMax(1,0);
+        getMax(0,0);
         System.out.println(sum);
     }
 
     static void getMax(int day,int subSum){
+        if(day>=n){
+            sum = Math.max(sum,subSum);
+            return;
+        }
+        if(day+schedule.get(day).time<=n){
+            getMax(day+schedule.get(day).time,subSum+schedule.get(day).pay);
+        }
+       //되는 거면 getMax()호출해야 함으로 !
+       getMax(day+1,subSum);
+    }
+}
+class Consult{
+    int time;
+    int pay;
 
+    public Consult(int time, int pay) {
+        this.time = time;
+        this.pay = pay;
     }
 }
