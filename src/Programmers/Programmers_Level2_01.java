8cd724cc85429a366a36332f033073a94d05af6e
유영우 <fbduddn97@naver.com>
2021-09-28 20:58:04 +0900

    [add] 오픈채팅방 named 02 in Programmers

--- src/Programmers/Programmers_Level2_01.java
+++ src/Programmers/Programmers_Level2_01.java
@@ -4,53 +4,53 @@ package Programmers;
 // 더 짧은 문자열로 줄여서 표현하는 알고리즘을 공부
 // 1개 이상 단위로 문자열을 잘라 압축하여 표현한 문자열 중 가장 짧은 것의 길이를 return
 
-class Solution {
-    public static int solution(String s) {
-        int answer = 0;
-        answer = s.length();
-        int unit = 1;
-        while(s.length()-unit>=unit){
-            int idx = 0;
-            String subAns="";
-            for(int i=idx; i<s.length(); i++){
-                if(idx>=s.length() || idx+unit>s.length()){
-                    if(idx<s.length()){
-                        subAns+=s.substring(idx,s.length());
-                    }
-                    break;
-                }
-                String str = s.substring(idx,idx+unit);
-
-                int comIdx = idx+unit;
-                int conCnt = 1;
-                for(int j=comIdx; j<s.length(); j++){
-                    if(comIdx>=s.length() || comIdx+unit > s.length()){
-                        break;
-                    }
-                    if(str.equals(s.substring(comIdx,comIdx+unit))){
-                        conCnt++;
-                    }else{
-
-                        break;
-                    }
-                    comIdx = comIdx+unit;
-                }
-                if(conCnt>1){
-                    subAns+=Integer.toString(conCnt)+str;
-                }else{
-                    subAns+=str;
-                }
-                idx=idx+unit*conCnt;
-            }
-            answer = Math.min(answer,subAns.length());
-            unit++;
-        }
-
-        return answer;
-    }
-}
 
 public class Programmers_Level2_01 {
+    private static class Solution {
+        public static int solution(String s) {
+            int answer = 0;
+            answer = s.length();
+            int unit = 1;
+            while(s.length()-unit>=unit){
+                int idx = 0;
+                String subAns="";
+                for(int i=idx; i<s.length(); i++){
+                    if(idx>=s.length() || idx+unit>s.length()){
+                        if(idx<s.length()){
+                            subAns+=s.substring(idx,s.length());
+                        }
+                        break;
+                    }
+                    String str = s.substring(idx,idx+unit);
+
+                    int comIdx = idx+unit;
+                    int conCnt = 1;
+                    for(int j=comIdx; j<s.length(); j++){
+                        if(comIdx>=s.length() || comIdx+unit > s.length()){
+                            break;
+                        }
+                        if(str.equals(s.substring(comIdx,comIdx+unit))){
+                            conCnt++;
+                        }else{
+
+                            break;
+                        }
+                        comIdx = comIdx+unit;
+                    }
+                    if(conCnt>1){
+                        subAns+=Integer.toString(conCnt)+str;
+                    }else{
+                        subAns+=str;
+                    }
+                    idx=idx+unit*conCnt;
+                }
+                answer = Math.min(answer,subAns.length());
+                unit++;
+            }
+
+            return answer;
+        }
+    }
     public static void main(String[] args){
         System.out.println(Solution.solution("xababcdcdababcdcd"));
     }
