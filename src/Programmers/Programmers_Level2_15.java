0b1e72d66612db766c2b3b92b9ef678aad0e712c
유영우 <fbduddn97@naver.com>
2021-10-15 19:10:01 +0900

    [add] 메뉴 리뉴얼 named 15 in Programmers

--- src/Programmers/Programmers_Level2_15.java
+++ src/Programmers/Programmers_Level2_15.java
@@ -11,101 +11,82 @@ package Programmers;
 // orders: 각 손님들이 주문한 단품메뉴들이 문자열 형식으로 담긴 배열
 // course: 스카피가 추가하고 싶어하는 코스요리를 구성하는 단품메뉴들의 갯수가 담긴 배열
 
+// 각 코스 개수에서 가장 많이 주문된 코스 뽑기
 // 각 코스요리 메뉴의 구성을 문자열 형식으로 배열에 담아 사전 순으로 오름차순 정렬해서 return
 
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.Map;
-//import java.util.TreeMap;
-//
-//class Solution {
-//    static HashMap<String, Integer> subAns = new HashMap<>();
-//
-//    static TreeMap<String, Integer> ans = new TreeMap<>();
-//    static int count;
-//
-//    public String[] solution(String[] orders, int[] course) {
-//        String[] answer = {};
-//
-//        for (int k = 0; k < orders.length; k++) {
-//            // 2메뉴로 쪼갠다.
-//            String order = orders[k];
-//            HashMap<String, Integer> orderr = new HashMap<>();
-//            for (int i = 0; i < order.length() - 1; i++) {
-//                for (int j = i + 1; j < orders[k].length(); j++) {
-//                    String twoCourse = String.valueOf(order.charAt(i)) + String.valueOf(order.charAt(j));
-//                    orderr.put(twoCourse, j);
-//                    if (subAns.containsKey(twoCourse)) {
-//                        ans.put(twoCourse, j);
-//                    }
-//                    subAns.put(twoCourse, j);
-//                }
-//            }
-//
-//            for (Map.Entry<String, Integer> entry : orderr.entrySet()){
-//                System.out.print(entry.getKey()+" ");
-//            }
-//            System.out.println();
-//
-//            for (int num : course) {
-//                if (num == 2 || order.length()<num) continue;
-//                orderr = slice(orderr, order, num);
-//                System.out.println();
-//            }
-//
-//            for (Map.Entry<String, Integer> entry : ans.entrySet()){
-//                System.out.print(entry.getKey()+" ");
-//            }
-//
-//        }
-//        return answer;
-//    }
-//
-//    // 한 손님의 오더들을 두개 씩 자른 오더들을 3,4 ... 만큼 쪼개서 subAns에 넣고 같은 키 값이 있으면 ans에 넣는다
-//    public HashMap<String, Integer> slice(HashMap<String, Integer> orderr, String order, int num) {
-//        HashMap<String, Integer> result = null;
-//        boolean conti = false;
-//        // orderr에 있는 key 값에 value 부터 order.length 만큼 돌면서 문자를 붙여준다.
-//        // 붙은 새로운 문자를 subAns에 넣고 subAns에 같은 값이 있으면 ans에 넣어준다.
-//        while(!conti){
-//            result = new HashMap<>();
-//            for (Map.Entry<String, Integer> entry : orderr.entrySet()){
-//                String or = entry.getKey();
-//                int idx = entry.getValue() + 1;
-//                if(or.length()+1 == num || idx>=or.length()) conti = true;
-//
-//                for (int i = idx; i < order.length(); i++) {
-//                    String orr = or + String.valueOf(order.charAt(idx)); // orr= AB -> or = ABC
-//                    result.put(orr, i);
-//                    if(conti){
-//                        if (subAns.containsKey(orr)) {
-//                            ans.put(orr, i);
-//                        }
-//                        subAns.put(orr, i);
-//                    }
-//                    idx++;
-//                }
-//            }
-////            System.out.println();
-//
-//            orderr = result;
-//        }
-//        for (Map.Entry<String, Integer> entry : result.entrySet()){
-//            System.out.print(entry.getKey()+" ");
-//        }
-//        return result;
-//    }
-//}
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 
-//public class Programmers_Level2_15 {
-//    public static void main(String[] args){
-//        String[] orders = {"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"};
-//        int[] course = {2,3,4};
-//
-//       String[] result =  new Solution().solution(orders,course);
-//
-//       for(String res: result){
-//           System.out.print(res+" ");
-//       }
-//    }
-//}
+class Solution {
+    static int targetNum;
+    static HashMap<String,Integer> map; // 모든 손님의 주문에 대해서 코스 개수 마다 코스가 주문이 얼마나 됐는지를 위한
+    static int max;
+    static PriorityQueue<String> priorityQueue = new PriorityQueue<>();
+    public String[] solution(String[] orders, int[] course) {
+
+        for(int num: course){
+            targetNum = num;
+            map = new HashMap<>();
+            max = 0;
+
+            // 1. 손님마다 가 주문들을 num으로 쪼개서 hashmap 에 주문 횟수를 넣는다
+            for(String order: orders){
+                selectMenuByNum(order,0,"");
+            }
+
+            // 2. 코스 개수마다 가장 많이 주문된 코스(들)을 queue 에 넣는다
+            for(Map.Entry<String, Integer> entry: map.entrySet()){
+                if(entry.getValue() == max && max>1){
+//                    System.out.print(entry.getKey()+" ");
+                    priorityQueue.offer(entry.getKey());
+                }
+            }
+//            System.out.println();
+        }
+
+        // 3. queue 의 값들을 answer 에 옮긴다
+        String[] answer = new String[priorityQueue.size()];
+        int cnt = 0;
+        while (!priorityQueue.isEmpty()){
+            answer[cnt++] = priorityQueue.poll();
+        }
+        return answer;
+    }
+
+    public void selectMenuByNum(String order, int idx, String menu){
+        if(menu.length() == targetNum){
+            char[] mn = menu.toCharArray();
+            Arrays.sort(mn);
+            menu = String.copyValueOf(mn);
+
+//            if(map.containsKey(menu)){
+//                int value = map.get(menu);
+//                map.put(menu,value+1);
+//            }else{
+//                map.put(menu,1);
+//            }
+
+            map.put(menu,map.getOrDefault(menu,0)+1);
+            max = Math.max(max,map.get(menu));
+            return;
+        }
+        for(int i=idx;i<order.length();i++){
+            selectMenuByNum(order,i+1,menu+String.valueOf(order.charAt(i)));
+        }
+    }
+}
+
+public class Programmers_Level2_15 {
+    public static void main(String[] args){
+        String[] orders = {"XYZ", "XWY", "WXA"};
+        int[] course = {2,3,4};
+
+       String[] result =  new Solution().solution(orders,course);
+
+       for(String res: result){
+           System.out.print(res+" ");
+       }
+    }
+}
