2950c4a15b2f333f3859f5dc3d0b8b4ccb898fcc
유영우 <fbduddn97@naver.com>
2021-10-18 01:25:03 +0900

    [add] 가장 큰수 named 19 in Programmers

--- src/Programmers/Programmers_Level2_18.java
+++ src/Programmers/Programmers_Level2_18.java
@@ -10,35 +10,33 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-class Solution {
-    public boolean solution(String[] phone_book) {
-        Set<String> set = new HashSet<>();
-        boolean answer = true;
+public class Programmers_Level2_18 {
+    private static class Solution {
+        public boolean solution(String[] phone_book) {
+            Set<String> set = new HashSet<>();
+            boolean answer = true;
 
-        Arrays.sort(phone_book,(a,b)->{return a.length()-b.length();});
-        set.add(phone_book[0]);
+            Arrays.sort(phone_book,(a,b)->{return a.length()-b.length();});
+            set.add(phone_book[0]);
 
-        for(int i=1; i<phone_book.length; i++){
-            String com = "";
-            for(int j=0; j<phone_book[i].length(); j++){
-                com+=String.valueOf(phone_book[i].charAt(j));
-                if(set.contains(com)){
-                    answer = false;
+            for(int i=1; i<phone_book.length; i++){
+                String com = "";
+                for(int j=0; j<phone_book[i].length(); j++){
+                    com+=String.valueOf(phone_book[i].charAt(j));
+                    if(set.contains(com)){
+                        answer = false;
+                        break;
+                    }
+                }
+                if(!answer){
                     break;
                 }
+                set.add(phone_book[i]);
             }
-            if(!answer){
-                break;
-            }
-            set.add(phone_book[i]);
+
+            return answer;
         }
-
-        return answer;
     }
-}
-
-
-public class Programmers_Level2_18 {
     public static void main(String[] args){
         String[] pB = {"119", "97674223", "1195524421"};
 
---
+++ src/Programmers/Programmers_Level2_19.java
@@ -0,0 +1,35 @@
+package Programmers;
+
+
+// 0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 찾아라.
+
+import java.util.Arrays;
+
+public class Programmers_Level2_19 {
+    private static class Solution {
+        public String solution(int[] numbers) {
+            String answer = "";
+
+            String[] result = new String[numbers.length];
+            for(int i=0; i<numbers.length; i++){
+                result[i] = String.valueOf(numbers[i]);
+            }
+
+            Arrays.sort(result,(a,b) -> (b+a).compareTo(a+b));
+
+            if(result[0].equals("0")){
+                answer = "0";
+            }else{
+                for(String a: result){
+                    answer+=a;
+                }
+            }
+            return answer;
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(new Solution().solution(new int[]{3, 30, 34, 5, 9}));
+
+    }
+}
