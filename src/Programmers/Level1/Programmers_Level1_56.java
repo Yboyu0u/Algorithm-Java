6607f132c2041a5a1e304bd5d3c3ba58b7594a8c
유영우 <fbduddn97@naver.com>
2021-11-04 19:37:56 +0900

    [add] 부족한 금액 계산하기 named 56 in Programmers

---
+++ src/Programmers/Level1/Programmers_Level1_56.java
@@ -0,0 +1,22 @@
+package Programmers.Level1;
+
+// 놀이기구의 원래 이용료는 price 인데, 용
+// 놀이기구를 N 번 째 이용한다면 원래 이용료의 N배를 받기로 하였다.
+
+// 놀이기구를 count번 타게 되면 현재 자신이 가지고 있는 금액에서 얼마가 모자라는지를 return
+class Solution {
+    public long solution(int price, int money, int count) {
+        long total=0;
+        while(count>0){
+            total+=price*count;
+            count--;
+        }
+        long answer = -1;
+        if(total-money<0) answer = 0;
+        else answer = total-money;
+        return answer;
+    }
+}
+
+public class Programmers_Level1_56 {
+}
