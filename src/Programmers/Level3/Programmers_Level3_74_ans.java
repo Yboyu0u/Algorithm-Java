216941a32cb5848edd7eb42b69ea251872f9f024
유영우 <fbduddn97@naver.com>
2021-11-16 20:57:05 +0900

    [add] 고양이와 개는 몇 마리 있을까  in SQL

--- src/Programmers/Level3/Programmers_Level3_74_ans.java
+++ src/Programmers/Level3/Programmers_Level3_74_ans.java
@@ -1,48 +1,47 @@
 package Programmers.Level3;
 
 
-class Solution{
-    public long solution(int n, int[] works){
-        int[] result = new int[works.length];
-        int totalovertime = sumArr(works)-n;
-        int time = totalovertime;
-
-        if(time<=0) return 0;
-
-        for(int i=0; i<works.length; i++){
-            result[i] = (works[i] < totalovertime/works.length) ? works[i] : totalovertime/works.length;
-            time-=result[i];
-        }
-
-        for(int i=0; i<works.length; i++){
-            if(time<=0) break;
-            if(works[i] > result[i]){
-                result[i]++;
-                time--;
-            }
-            if(i == works.length-1) i=-1;
-        }
-
-        return getTiredness(result);
-    }
-
-    long getTiredness(int[] works){
-        long sum=0;
-        for(int work : works){
-            sum+=work*work;
-        }
-        return sum;
-    }
-
-    int sumArr(int[] works){
-        int sum=0;
-        for(int work: works){
-            sum+=work;
-        }
-        return sum;
-    }
-}
-
-
 public class Programmers_Level3_74_ans {
+    private static class Solution{
+        public long solution(int n, int[] works){
+            int[] result = new int[works.length];
+            int totalovertime = sumArr(works)-n;
+            int time = totalovertime;
+
+            if(time<=0) return 0;
+
+            for(int i=0; i<works.length; i++) {
+                result[i] = (works[i] < totalovertime / works.length) ? works[i] : totalovertime / works.length;
+                time -= result[i];
+            }
+
+
+            for(int i=0; i<works.length; i++){
+                if(time<=0) break;
+                if(works[i] > result[i]){
+                    result[i]++;
+                    time--;
+                }
+                if(i == works.length-1) i=-1;
+            }
+
+            return getTiredness(result);
+        }
+
+        long getTiredness(int[] works){
+            long sum=0;
+            for(int work : works){
+                sum+=work*work;
+            }
+            return sum;
+        }
+
+        int sumArr(int[] works){
+            int sum=0;
+            for(int work: works){
+                sum+=work;
+            }
+            return sum;
+        }
+    }
 }
