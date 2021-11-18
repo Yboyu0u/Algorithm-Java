597e02d49483c672f1b177cc507e620e96ad2ea5
유영우 <fbduddn97@naver.com>
2021-11-18 16:46:39 +0900

    [add] 있었는데요 없었습니다 in SQL

---
+++ src/SQL/Level3/있었는데요 없었습니다.sql
@@ -0,0 +1,5 @@
+-- 보호 시작일 ings 보다 입양일 outs 이 더 빠른 동물의 아이디와 이름을 조회
+
+SELECT i.ANIMAL_ID, i.NAME FROM ANIMAL_INS i
+JOIN ANIMAL_OUTS o ON i.ANIMAL_ID=o.ANIMAL_ID
+WHERE i.DATETIME>o.DATETIME ORDER BY i.DATETIME;
