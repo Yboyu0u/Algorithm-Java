48f658b2eaca98b0bb62f4e3c39a023043f67390
유영우 <fbduddn97@naver.com>
2021-11-30 17:29:47 +0900

    [add!!] 2개 이하로 다른 비트 named 87 in Programmers

--- src/Programmers/Level2/Programmers_Level2_87.java
+++ src/Programmers/Level2/Programmers_Level2_87.java
@@ -2,63 +2,45 @@ package Programmers.Level2;
 
 // f(x) = x 보다 크고 x 와 비트가 1~2개 다른 수들 중에서 제일 작은 수
 
-// class Solution {
-// 	public long[] solution(long[] numbers) {
-// 		long[] answer = new long[numbers.length];
-// 		int idx = 0;
-//
-// 		// 1. 각 number 의 f(x)값 구하기
-// 		for (int i = 0; i < numbers.length; i++) {
-// 			long num = numbers[i];
-// 			// 2. f(x) 값 구해서 answer 에 넣기
-// 			answer[idx++] = findFx(num);
-// 		}
-//
-// 		return answer;
-// 	}
-//
-// 	public long findFx(long num) {
-// 		StringBuilder biNum = new StringBuilder(Long.toBinaryString(num));
-//
-// 		if(num%2 == 0){
-// 			return num+1;
-// 		}
-//
-// 		else{
-// 			long fNum = num;
-//
-// 			boolean check = false;
-// 			while (!check) {
-// 				fNum++;
-// 				String fBiNum = Long.toBinaryString(fNum);
-//
-// 				if (fBiNum.length() != biNum.length()) {
-// 					biNum.insert(0,'0');
-// 				}
-//
-// 				int cnt = 0;
-// 				for (int i = fBiNum.length() - 1; i >= 0; i--) {
-// 					if (biNum.charAt(i) != fBiNum.charAt(i)) {
-// 						cnt++;
-// 					}
-//
-// 					if (cnt > 2 ) {
-// 						break;
-// 					}
-//
-// 				}
-//
-// 				if (cnt == 2 || cnt == 1) {
-// 					check = true;
-// 				}
-// 			}
-//
-// 			return fNum;
-// 		}
-// 	}
-// }
-
 public class Programmers_Level2_87 {
+	private static class Solution {
+		public long[] solution(long[] numbers) {
+			long[] answer = new long[numbers.length];
+			int idx = 0;
+
+			// 1. 각 number 의 f(x)값 구하기
+			for (int i = 0; i < numbers.length; i++) {
+				long num = numbers[i];
+				// 2. f(x) 값 구해서 answer 에 넣기
+				answer[idx++] = findFx(num);
+			}
+
+			return answer;
+		}
+
+		public long findFx(long num) {
+			String biNum = Long.toBinaryString(num);
+
+			if (num % 2 == 0) {
+				return num + 1;
+			}
+
+			if(num % 2 == 1) {
+				if (biNum.contains("0")) {
+					for (int i = biNum.length() - 1; i >= 0; i--) {
+						if (biNum.charAt(i) == '0') {
+							biNum = biNum.substring(0, i) + "10" + biNum.substring(i + 2);
+							break;
+						}
+					}
+				}
+				else {
+					biNum = biNum.substring(0, 1) + "0" + biNum.substring(1);
+				}
+			}
+			return Long.parseLong(biNum, 2);
+		}
+	}
 	public static void main(String[] args) {
 		long[] numbers = {7};
 
